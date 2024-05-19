0c29e09c2a1b13c20668ffe41addd5e1d3e13fda
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2024-05-19 14:03:32 +0200

    refactor: changes in getPlayersWithMostWins() in TwoPlayerGameResultManager suggested by Robi

--- src/main/java/gameresult/manager/TwoPlayerGameResultManager.java
+++ src/main/java/gameresult/manager/TwoPlayerGameResultManager.java
@@ -3,28 +3,28 @@ package gameresult.manager;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import static java.util.stream.Collectors.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import game.State.Status;
 import gameresult.TwoPlayerGameResult;
 
 public interface TwoPlayerGameResultManager extends GameResultManager<TwoPlayerGameResult> {
 
     default List<Wins> getPlayersWithMostWins(int limit) throws IOException {
-        Map<String, Long> winsByPlayerName = getAll().stream()
-                .filter(result -> result.getStatus() == Status.PLAYER_1_WINS || result.getStatus() == Status.PLAYER_2_WINS)
+        return getAll().stream()
                 .map(result -> switch (result.getStatus()) {
-                    case PLAYER_1_WINS -> result.getPlayer1Name();
-                    case PLAYER_2_WINS -> result.getPlayer2Name();
-                    default -> throw new AssertionError();
+                    case PLAYER_1_WINS -> Optional.of(result.getPlayer1Name());
+                    case PLAYER_2_WINS -> Optional.of(result.getPlayer2Name());
+                    default -> Optional.<String>empty();
                 })
-                .collect(groupingBy(Function.identity(), counting()));
-        return winsByPlayerName.entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .flatMap(Optional::stream)
+                .collect(groupingBy(Function.identity(), counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String,Long>comparingByValue().reversed())
                 .limit(limit)
                 .map(entry -> new Wins(entry.getKey(), entry.getValue()))
                 .toList();
